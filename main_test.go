6efb54e9156f78fe4f94200e7165298bc6a4acf7
IonovEgor <ionov.egor@gmail.com>
2024-12-29 14:51:00 +0300

    second iteration

--- main_test.go
+++ main_test.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
-func TestMainHandler(t *testing.T) {
+func TestMainHandlerWhenOkAndNotEmptyBody(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+
+	assert.NotEmpty(t, responseRecorder.Body)
+}
+
+func TestMainHandlerWhenWrongCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=london", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
+}
+
+func TestMainHandlerIsReturnAll(t *testing.T) {
 	totalCount := 4
 	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
@@ -17,11 +39,7 @@ func TestMainHandler(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.NotEqual(t, responseRecorder.Code, http.StatusBadRequest)
-	assert.NotEmpty(t, responseRecorder.Body)
-
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
+	list := strings.Split(responseRecorder.Body.String(), ",")
 
 	assert.Len(t, list, totalCount)
 }
